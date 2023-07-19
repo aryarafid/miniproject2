f9b73d9a9670d2d8534c04d8cf090736b15e8026
aryarafid <aryamohamaddanendra@gmail.com>
2023-07-19 13:30:46 +0700

    get rid of less important things lol

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -7,50 +7,6 @@ const sequelize = db.Sequelize;
 const { Op } = sequelize;
 
 const blogController = {
-  // related to country
-  getAllCountry: async (req, res) => {
-    try {
-      const result = await Country.findAll();
-      return res.status(200).json({
-        message: "Get data success",
-        data: result,
-      });
-    } catch (error) {
-      return res.status(500).json({
-        message: "Get data failed",
-        error: error.message,
-      });
-    }
-  },
-
-  getCountryById: async (req, res) => {
-    try {
-      // ambil id dari parameter path url
-      const { id } = req.query;
-
-      // return res.send(id);
-
-      // select * from branches b, users u where b.id = u.id
-      const result = await Country.findOne({
-        where: {
-          id,
-        },
-        // eager loading
-      });
-
-      return res.status(200).json({
-        message: "Get data success",
-        data: result,
-      });
-    } catch (err) {
-      // return function if fail or error
-      return res.status(500).json({
-        message: "Get data failed",
-        error: err.message,
-      });
-    }
-  },
-
   getAllBlog: async (req, res) => {
     try {
       const result = await Blog.findAndCountAll({
